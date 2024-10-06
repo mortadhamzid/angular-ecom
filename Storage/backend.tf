55d4fd284409f3c2c62f7f03de726ce0097a4454
mortadhamzid <117196622+mortadhamzid@users.noreply.github.com>
2024-10-06 11:37:10 +0100

    Update backend.tf

--- Storage/backend.tf
+++ Storage/backend.tf
@@ -4,8 +4,8 @@ terraform {
     storage_account_name  = "stoarageacc177"
     container_name        = "tfstate"
     key                   = "app-1-aks-d.tfstate"
-    arm_client_id         = "ed4b67b9-3f8c-4557-a031-fbb7bb765f79"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    arm_subscription_id   = "90e9e45d-3b55-41f9-b350-aef4bb03ea3c"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    arm_tenant_id         = "9680b44d-d7ce-46a6-bf0d-6a6b6ec1ae9a"
+    #arm_client_id         = "ed4b67b9-3f8c-4557-a031-fbb7bb765f79"  # Can also be set via `ARM_CLIENT_ID` environment variable.
+    #arm_subscription_id   = "90e9e45d-3b55-41f9-b350-aef4bb03ea3c"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    #arm_tenant_id         = "9680b44d-d7ce-46a6-bf0d-6a6b6ec1ae9a"
   }
 }
