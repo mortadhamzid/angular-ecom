43573b220240796cb78df38d03de8d3a1638ab3c
mortadhamzid <117196622+mortadhamzid@users.noreply.github.com>
2024-10-06 14:39:11 +0100

    Update backend.tf

--- Storage/backend.tf
+++ Storage/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "landing_zone"
-    storage_account_name  = "stoarageacc177"
-    container_name        = "tfstate"
-    key                   = "app-1-aks-d.tfstate"
+#terraform {
+ # backend "azurerm" {
+  #  resource_group_name   = "landing_zone"
+   # storage_account_name  = "stoarageacc177"
+    #container_name        = "tfstate"
+    #key                   = "app-1-aks-d.tfstate"
 
-  }
-}
+  #}
+#}
